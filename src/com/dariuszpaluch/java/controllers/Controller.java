fdc39853c1645845a6474dbf27df2e43dd1e3916
Dariusz Paluch <dariusz@dariuszpaluch.com>
2018-01-07 20:45:03 +0100

    Init project

---
+++ src/com/dariuszpaluch/java/controllers/Controller.java
@@ -0,0 +1,4 @@
+package com.dariuszpaluch.java.controllers;
+
+public class Controller {
+}
---
+++ src/com/dariuszpaluch/java/controllers/MainLayoutController.java
@@ -0,0 +1,14 @@
+package com.dariuszpaluch.java.controllers;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+/**
+ * Created by Dariusz Paluch on 07.01.2018.
+ */
+public class MainLayoutController {
+  public TextField login;
+  public TextField password;
+  public Button registrationButton;
+  public Button loginButton;
+}
